d6cc003539761c1eda02d09850de2ef838a80532
Sitong <nickren93@gmail.com>
2024-10-25 21:03:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.round((Math.random()*10)) + 1
+
+const num3 = 4;
+const num4 = 5;
+let mod = num3 % num4;
+
+const num5 = -3.4
+const num6 = 20
+const max = Math.max(num1, num3, num4, num5, num6);
